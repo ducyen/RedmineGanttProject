99f36e0f27a77e1a579d609816cddd7f725bd7c5
Hoang Duc <Hoang.Duc@exc.epson.co.jp>
2017-01-17 09:01:52 +0900

    Fixed sample batch files for proxy server

--- samples/DemoRedmine/DemoRedmine_Save_Ldr.bat
+++ samples/DemoRedmine/DemoRedmine_Save_Ldr.bat
@@ -1,9 +1,11 @@
 echo off
+rem set JAVA_FLAGS=-Dhttp.proxyHost=10.0.201.201 -Dhttp.proxyPort=8080
+set JAVA_FLAGS=
 set API_KEY=bbda35ce24663d405685a13f9f2403f56db7f98b
-set REDMINE_URL=http://www.hostedredmine.com
+set REDMINE_URL=http://www.hostedredmine.com/
 set GANTTPROJ_FILE=DemoRedmine.gan
 set PROJECTS=redmineganttproject
 set ASSIGNEE=*
 set START_DATE=2015-11-01
-java -jar ..\..\release\RedmineToGanttProj.jar save
+java %JAVA_FLAGS% -jar ..\..\release\RedmineToGanttProj.jar save
 pause
