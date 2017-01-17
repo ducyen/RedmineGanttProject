b91c7d279ee2e8f7fd0a1348657bf68a9f89017c
Hoang Duc <Hoang.Duc@exc.epson.co.jp>
2017-01-17 10:01:11 +0900

    Remove proxy settings

--- samples/BuildAHouse/BuildAHouse_Common.bat
+++ samples/BuildAHouse/BuildAHouse_Common.bat
@@ -1,6 +1,6 @@
 echo off
-set JAVA_FLAGS=-Dhttp.proxyHost=10.0.201.201 -Dhttp.proxyPort=8080
-rem set JAVA_FLAGS=
+rem set JAVA_FLAGS=-Dhttp.proxyHost=10.0.201.201 -Dhttp.proxyPort=8080
+set JAVA_FLAGS=
 set API_KEY=bbda35ce24663d405685a13f9f2403f56db7f98b
 set REDMINE_URL=http://www.hostedredmine.com/
 set GANTTPROJ_FILE=BuildAHouse.gan
